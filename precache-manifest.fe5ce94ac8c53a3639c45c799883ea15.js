0eb95e7ced728eb2e7d86d2f3476788e5535dd86
Tayna-Macedo <tayna_sm1996@hotmail.com>
2022-07-11 11:33:38 -0300

    Updates

---
+++ precache-manifest.fe5ce94ac8c53a3639c45c799883ea15.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "791e34ff242ab56a417709b59f16aa68",
+    "url": "/project-trivia/index.html"
+  },
+  {
+    "revision": "fea6a6a09ede3ced7946",
+    "url": "/project-trivia/static/css/main.120fb906.chunk.css"
+  },
+  {
+    "revision": "a71db7069aaf4dc2a7a6",
+    "url": "/project-trivia/static/js/2.07901dcc.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/project-trivia/static/js/2.07901dcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fea6a6a09ede3ced7946",
+    "url": "/project-trivia/static/js/main.9ec7fa84.chunk.js"
+  },
+  {
+    "revision": "f125377fa2c39526d4b3",
+    "url": "/project-trivia/static/js/runtime-main.33b57c0b.js"
+  },
+  {
+    "revision": "4bbb1d74ef0ac4f1d6784c3a6ff4b2a2",
+    "url": "/project-trivia/static/media/brainhappy.4bbb1d74.png"
+  },
+  {
+    "revision": "0e26df6aa55b13bec7a85c7a7c6fb923",
+    "url": "/project-trivia/static/media/brainsad.0e26df6a.png"
+  },
+  {
+    "revision": "466d153e266e6e813c5ef2c55aa5fc2c",
+    "url": "/project-trivia/static/media/trivia.466d153e.png"
+  }
+]);
